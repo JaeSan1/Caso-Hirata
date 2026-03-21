e6938c51d9b16a7241a66e9f3a6fc7595a4131ea
Jason <munozjason532@gmail.com>
2026-03-21 12:31:14 -0400

    Actualización de lógica en clase Transporte

--- Modelo/Transporte.java
+++ Modelo/Transporte.java
@@ -1,5 +1,90 @@
 package Modelo;
 
-public class Transporte {
-    
+//import java.sql.Date;
+
+
+public class conductores {
+    private int id;
+    private String nombre;
+    private String licencia;
+    private int telefono;
+
+    public conductores(int id, String nombre, String licencia, int telefono){
+        this.id = id;
+        this.nombre = nombre;
+        this.licencia = licencia;
+        this.telefono = telefono;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
+
+public class camiones {
+    private int id;
+    private String marca;
+    private String modelo;
+    private int anio;
+    private double km_actual;
+    private double km_ultimo_mantenimiento;
+
+    // Constructor
+    public camiones(int id, String marca, String modelo, int anio, double km_actual, double km_ultimo_mantenimiento) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo =modelo;
+        this.anio = anio;
+        this.km_actual =km_actual;
+        this.km_ultimo_mantenimiento = km_ultimo_mantenimiento;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getmarca() {
+        return marca;
+    }
+
+    public void setmarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getConductor() {
+        return modelo;
+    }
+
+    public void setmodelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getanio() {
+        return anio;
+    }
+
+    public void setanio(int anio) {
+        this.anio = anio;
+    }
+
+    public double iskm_actual() {
+        return km_actual;
+    }
+
+    public void setkm_actual(double km_actual) {
+        this.km_actual = km_actual;
+    }
+
+    public double getkm_ultimo_mantenimiento() {
+        return km_ultimo_mantenimiento;
+    }
+
+    public void setkm_ultimo_mantenimiento(double km_ultimo_mantenimiento) {
+        this.km_ultimo_mantenimiento = km_ultimo_mantenimiento;
+    }
 }
